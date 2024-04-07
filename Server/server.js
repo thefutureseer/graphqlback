099f785749ce80c4a9ee73ccf66e561870089f1d
thefutureseer <danielharrislimos@outlook.com>
2024-04-07 07:21:22 -0700

    add express to apollo

--- Server/server.js
+++ Server/server.js
@@ -1,13 +1,48 @@
-//bring in apollo server
-const {ApolloServer} = require('apollo-server');
-
 // bring in typedefs and resolvers
 const {typeDefs, resolvers} = require('../graphql/index');
+const { ApolloServer } = require( '@apollo/server');
+const { expressMiddleware } = require( '@apollo/server/express4');
+const { ApolloServerPluginDrainHttpServer } = require( '@apollo/server/plugin/drainHttpServer');
+const express = require( 'express');
+const http = require( 'http');
+const cors = require( 'cors');
+const bodyParser = require( 'body-parser');
 
-const server = new ApolloServer({
-  typeDefs, resolvers,
+// // The GraphQL schema
+// const typeDefs = `#graphql
+//   type Query {
+//     hello: String
+//   }
+// `;
+
+// // A map of functions which return data for the schema.
+// const resolvers = {
+//   Query: {
+//     hello: () => 'world',
+//   },
+// };
+
+const app = express();
+const httpServer = http.createServer(app);
+
+(async () => {
+// Set up Apollo Server
+var server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-server.listen().then(({url})=>{
-  console.log(`server ready on: ${url}` )
-});
+  await server.start();
+  
+  app.use(
+    cors(),
+    bodyParser.json(),
+    expressMiddleware(server),
+  );
+})();
+
+(async () =>{
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+console.log(`🚀 Server ready at http://localhost:4000`);
+})();
