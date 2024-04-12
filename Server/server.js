836376f834f09335514a461f10aecca1dac6e429
thefutureseer <danielharrislimos@outlook.com>
2024-04-11 22:20:46 -0700

    clean up server

--- Server/server.js
+++ Server/server.js
@@ -23,38 +23,3 @@ startServer().then(() => {
 }).catch(err => {
   console.error('Error starting server:', err);
 });  
-
-  // // bring in typedefs and resolvers
-// const {typeDefs, resolvers} = require('../graphql/index');
-// const { ApolloServer } = require( '@apollo/server');
-// const { expressMiddleware } = require( '@apollo/server/express4');
-// const { ApolloServerPluginDrainHttpServer } = require( '@apollo/server/plugin/drainHttpServer');
-// const express = require( 'express');
-// const http = require( 'http');
-// const cors = require( 'cors');
-// const bodyParser = require( 'body-parser');
-
-// const app = express();
-// const httpServer = http.createServer(app);
-
-// (async () => {
-// // Set up Apollo Server
-// var server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-//   plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-// });
-
-//   await server.start();
-  
-//   app.use(
-//     cors(),
-//     bodyParser.json(),
-//     expressMiddleware(server),
-//   );
-// })();
-
-// (async () =>{
-// await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
-// console.log(`🚀 Server ready at http://localhost:4000`);
-// })();
