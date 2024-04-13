45009956c03e2d960ef43887c02d847ad9497236
thefutureseer <danielharrislimos@outlook.com>
2024-04-13 03:23:02 -0700

    add RandomDie type

--- graphql/schema/typeDefs.js
+++ graphql/schema/typeDefs.js
@@ -1,12 +1,19 @@
 const {gql} = require('apollo-server');
 
 // // Construct a schema, using GraphQL schema language
-const typeDefs = gql`
+const typeDefs = gql(`
+  type RandomDie {
+    numSides: Int!
+    rollOnce: Int!
+    roll(numRolls: Int!): [Int]
+  }
+
   type Query {
     hello: String,
     rollDice(numDice: Int!, numSides: Int): [Int]
     calculateSum(a: Int!, b: Int!): Int!
+    getDie(numSides: Int!): RandomDie
   }
-  `
+`)
 
 module.exports = typeDefs;
